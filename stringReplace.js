a41c3f4171288371928a2c7633d93de655500e4e
Andrew Dong <andrewdong1994@gmail.com>
2018-10-11 16:10:51 -0400

    finished stringReplace

--- stringReplace.js
+++ stringReplace.js
@@ -5,7 +5,23 @@ console.log("String Replace");
 let str = "www.some characters and whitespace.com";
 
 function stringReplace(str) {
-  
+  // targetElCount = 0;
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] === " ") {
+  //     targetElCount ++;
+  //   }
+  // }
+
+  for (let i = str.length; i >0; i--) {
+    if (str[i] === " ") {
+      let remainder = str.slice(i+1);
+      let current = str.slice(0,i);
+      str = current + "%20" + remainder;
+    }
+  }
+
+  return str;
 }
 
 console.log(str);
+console.log(stringReplace(str));
